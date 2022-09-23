4a2ae2aa0969b3791b7a77d476664369db86c673
Alex Mandelias <alexmandelias@gmail.com>
2022-09-23 12:00:57 +0300

    adds c++ sandbox file

---
+++ maincpp.cpp
@@ -0,0 +1,18 @@
+
+// Author: Alex Mandelias
+
+// Build and Run
+// g++ -Wextra -o maincpp.exe maincpp.cpp && maincpp.exe
+
+// Build, Run and Clean
+// g++ -o maincpp.exe maincpp.cpp && maincpp.exe && del maincpp.exe
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
